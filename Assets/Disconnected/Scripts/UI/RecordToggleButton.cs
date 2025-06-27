6d32d79ae26af90bd04ab344a5d5f28fa9e41b2a
Tiago Silva <tiagomms.15@gmail.com>
2025-06-27 17:03:05 +0100

    Added DoTween for script animations, vr keyboard to scene

---
+++ Assets/Disconnected/Scripts/UI/RecordToggleButton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class RecordToggleButton : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Button button;
+    [SerializeField] private Image background;
+
+    [Header("Colors")]
+    [SerializeField] private Color recordingColor = Color.red;
+
+    [Header("Animation")]
+    [SerializeField] private float colorDuration = 0.3f;
+    [SerializeField] private float scalePunch = 0.2f;
+
+    private Color idleColor = Color.gray;
+    private bool isRecording = false;
+
+    private void Awake()
+    {
+        idleColor = background.color;
+    }
+    
+    public void ToggleRecording()
+    {
+        isRecording = !isRecording;
+
+        Color targetColor = isRecording ? recordingColor : idleColor;
+
+        // Animate color
+        background.DOColor(targetColor, colorDuration);
+
+        // Optional: Add a punch scale animation
+        background.transform.DOPunchScale(Vector3.one * scalePunch, 0.3f, vibrato: 4, elasticity: 0.7f);
+
+        // change color
+        //var nextButtonColor = isRecording ? button.colors.pressedColor : button.colors.normalColor;
+        //button.targetGraphic.DOColor(nextButtonColor, colorDuration);
+    }
+}
